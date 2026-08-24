1f4a9da2aab0ad96c63ec8c2f3d7771754dbd92e
Milo34 <BlairK34@Live.com>
2026-08-24 03:34:42 -0600

    Add files via upload
    Test

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
-const CACHE = 'meshdoctor-v1.5.3';
+const CACHE = 'meshdoctor-v1.5.4';
 const CORE = [
   './',
   './index.html',
-  './styles.css?v=1.5.3',
-  './app.js?v=1.5.3',
-  './config.js?v=1.5.3',
-  './manifest.webmanifest?v=1.5.3',
-  './icons/icon-192.png?v=1.5.3',
-  './icons/icon-512.png?v=1.5.3',
-  './assets/home-splash.png?v=1.5.3'
+  './styles.css?v=1.5.4',
+  './app.js?v=1.5.4',
+  './config.js?v=1.5.4',
+  './manifest.webmanifest?v=1.5.4',
+  './icons/icon-192.png?v=1.5.4',
+  './icons/icon-512.png?v=1.5.4',
+  './assets/home-splash.png?v=1.5.4'
 ];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(()=>self.skipWaiting()));
